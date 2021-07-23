a8bc812eae76714e8d4708df7a296376eb974787
FrankHsiao <lordarcher12345@gmail.com>
2021-07-23 17:14:15 +0800

    Test Asynchronous Success
    important*

--- bodyCanves.js
+++ bodyCanves.js
@@ -7,8 +7,11 @@ let context = canvas.getContext("2d");
 context.beginPath();
 context.font = "bold 12px Montserrat";
 
-// 之後作為非同步請求Ajax
+// 渲染數據
+// 之後作為非同步請求Ajax OK
+// 之後要做 取七天期間日期
 function renderExerciseData() {
+  
   for (let i = 0; i < 8; i++) {
     // 日期顏色
     context.fillStyle = "#343633"
@@ -17,11 +20,11 @@ function renderExerciseData() {
     // 抓近七天 ***
     let weekDays = ["8/6", "8/7", "8/8", "8/9", "8/10", "8/10", "8/11"];
     context.fillText(weekDays[i], intervalH + 35, 218);
-
+    
     // 長條圖顏色
     context.fillStyle = "#6460ff"
     // 長條圖 ***
-    let kaclData = [428, 526, 377, 668, 762, 536, 294];
+    // let kaclData = [428, 526, 377, 668, 762, 536, 294];
     // context.fillRect(X,Y,寬,消耗Kcal/5)
     let graphicLen = kaclData[i] / 5
     // 長條圖顯示範圍高 205 
@@ -32,39 +35,10 @@ function renderExerciseData() {
   }
 }
 
-function doFirst() {
-  // 繪製卡路里開始
-  for (let i = 0; i < 11; i++) {
-    // 間隔 (每100 kcal)
-    let intervalV = i * 20;
-
-    let kcalLevel = 1000;
-
-    //消耗kcal
-    //+n = 起始點
-    context.fillStyle = "#343633"
-    context.moveTo(26, intervalV + 5);
-    context.lineTo(canvas.width, intervalV + 5);
-    context.fillText(kcalLevel - (i * 100), 0, intervalV + 11);
-  }
-  // 渲染運動數據
-  renderExerciseData();
-
-  //橫線條
-  context.strokeStyle = "rgba(0,0,0,0.15)";
-  context.stroke();
-
-  // 標記 kcal
-  context.beginPath();
-  context.font = "bold 12px Montserrat";
-  context.fillStyle = "#343633"
-  context.fillText("kcal", 0, 228);
-  context.stroke();
-}
-
-//  To Server requset for UserDailyRecord
+// 非同步請求
+// To Server requset for UserDailyRecord
 let kaclData = [] ;
-let weekDays = [] ;
+// let weekDays = [] ;
 function getUserDailyRecord() {
   fetch(`/testGraphic`)
     .then(response => {
@@ -82,20 +56,50 @@ function getUserDailyRecord() {
         data[5].kcalBurned,
         data[6].kcalBurned];
       console.log(kaclData);
-      // 抓七天 日期
-      weekDays = [
-        data[0].date,
-        data[1].date,
-        data[2].date,
-        data[3].date,
-        data[4].date,
-        data[5].date,
-        data[6].date];
-      console.log(weekDays);
-    })
+      // // 抓七天 日期
+      // weekDays = [
+      //   data[0].date,
+      //   data[1].date,
+      //   data[2].date,
+      //   data[3].date,
+      //   data[4].date,
+      //   data[5].date,
+      //   data[6].date];
+      // console.log(weekDays);
+    }).then(() => renderExerciseData())
 }
 
-getUserDailyRecord();
+// window load 開始畫基本Canvas
+function doFirst() {
+  // 繪製卡路里開始
+  for (let i = 0; i < 11; i++) {
+    // 間隔 (每100 kcal)
+    let intervalV = i * 20;
+
+    let kcalLevel = 1000;
+
+    //消耗kcal
+    //+n = 起始點
+    context.fillStyle = "#343633"
+    context.moveTo(26, intervalV + 5);
+    context.lineTo(canvas.width, intervalV + 5);
+    context.fillText(kcalLevel - (i * 100), 0, intervalV + 11);
+  }
+
+  // 渲染數據
+  getUserDailyRecord();
+
+  //橫線條
+  context.strokeStyle = "rgba(0,0,0,0.15)";
+  context.stroke();
+
+  // 標記 kcal
+  context.beginPath();
+  context.font = "bold 12px Montserrat";
+  context.fillStyle = "#343633"
+  context.fillText("kcal", 0, 228);
+  context.stroke();
+}
 
 window.addEventListener("load", doFirst);
 
